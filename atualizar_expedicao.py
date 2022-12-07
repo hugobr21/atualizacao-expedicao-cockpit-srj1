3d15c04dab1d08b85e49fe549f068a062cf7f45c
Victor Sobral <victor.cont.prof@gmail.com>
2022-12-07 19:18:48 -0300

    ajustar loop

--- atualizar_expedicao.py
+++ atualizar_expedicao.py
@@ -1,6 +1,7 @@
 from google_api_functions import *
 from datetime import datetime 
 import pandas as pd
+import traceback
 import json
 import time
 import os
@@ -24,239 +25,84 @@ def apagarCSVs():
 		time.sleep(1)
 
 def baixarArquivoGestaoDePacotes(xpathcompleto):
+	apagarCSVs()
 	driver.get('https://envios.mercadolivre.com.br/logistics/management-packages')
-	nome_do_arquivo = 'C:\\Users\\' + os.getlogin() + '\\Downloads\\' + 'logistics_packages_' + '-'.join([time.strftime("%d"),time.strftime("%m"),time.strftime("%Y")]) + '.csv'
-	print(nome_do_arquivo)
-	contador = 0
+
 	# Baixa o arquivo
 	while True:
 		time.sleep(5)
 		try:
 			time.sleep(1)
-			driver.find_element(By.XPATH,'//*[@id="packages-management"]/div[1]/button').click()
-			time.sleep(2)
-			driver.find_element(By.XPATH,xpathcompleto).click()
-			for i in range(10):
+			driver.find_element(By.XPATH,'/html/body/main/div/div[2]/div/div/div[1]/button/span').click()
+			
+			# Loop para clicar no botão do arquivo
+			for i in range(20):
 				time.sleep(1)
 				try:
-					driver.find_element(By.XPATH,'/html/body/main/div/div[2]/div/div/div[2]/div[2]/button').click()
+					botaodoarquivo = driver.find_element(By.XPATH,xpathcompleto)
+					textodobotaodoarquivo = botaodoarquivo.text.split('\n')[0]
+					time.sleep(3)
+					botaodoarquivo.click()
+					if debug_mode:
+						print('Loop para clicar no botão do arquivo')
 					break
-				except:
+				except Exception as e:
+					if debug_mode:
+						print(traceback.format_exc())
 					pass
-			break	
-		except:
-			if contador >= 10:
-				print('#1 Reiniciando página e download')
-				driver.get("https://envios.mercadolivre.com.br/logistics/management-packages")
-				time.sleep(6)
-				# funcao_principal()
-			print('erro ao baixar arquivo')
-			contador = contador + 1
-			pass
-	contador = 0
-	# Carrega e trata o arquivo
-	while True:
-		time.sleep(1)
-		try:
-			os.chdir(f'C:\\Users\\{user_name}\\Downloads')
-			emrotadeentrega = pd.read_csv([nomesDosArquivos for nomesDosArquivos in os.listdir() if ('logistics_packages' in nomesDosArquivos) and ('.part' not in nomesDosArquivos)][0])
-			os.chdir(diretorio_robo)
-			emrotadeentrega['ID do envio'] = pd.to_numeric(emrotadeentrega['ID do envio'], errors='coerce')
-			emrotadeentrega = emrotadeentrega.loc[~ (emrotadeentrega['ID do envio'].isna())]
-			emrotadeentrega['ID do envio'] = emrotadeentrega['ID do envio'].astype('str').str[:11]
-			emrotadeentrega = emrotadeentrega.fillna('')
-			os.remove(nome_do_arquivo)
-			print('Arquivo em rota de entrega carregado')
-			return emrotadeentrega
-		except Exception as e:
-			print(e)
-			contador = contador + 1
-			if contador >=100:
-				try:
-					os.remove(nome_do_arquivo)
-				except:
-					pass
-				print('#2 Reiniciando página e download')
-				driver.get("https://envios.mercadolivre.com.br/logistics/management-packages")
-				time.sleep(6)
-				raise KeyError
-			# print('erro ao carregar arquivo')
-			pass
-
-def baixarEmRotaDeEntrega():
-	driver.get('https://envios.mercadolivre.com.br/logistics/management-packages')
-	nome_do_arquivo = 'C:\\Users\\' + os.getlogin() + '\\Downloads\\' + 'logistics_packages_' + '-'.join([time.strftime("%d"),time.strftime("%m"),time.strftime("%Y")]) + '.csv'
-	print(nome_do_arquivo)
-	contador = 0
-	# Baixa o arquivo
-	while True:
-		time.sleep(5)
-		try:
-			time.sleep(1)
-			driver.find_element(By.XPATH,'//*[@id="packages-management"]/div[1]/button').click()
-			time.sleep(2)
-			driver.find_element(By.XPATH,'//*[@id="packages-management"]/div[1]/div/div[2]/div/div[3]/div[3]').click()
-			for i in range(10):
+			
+			# Loop para clicar no botão de baixar o arquivo
+			for i in range(20):
 				time.sleep(1)
 				try:
-					driver.find_element(By.XPATH,'/html/body/main/div/div[2]/div/div/div[2]/div[2]/button').click()
+					botaobaixar = driver.find_element(By.XPATH,'/html/body/main/div/div[2]/div/div/div[2]/div[2]/button')
+					time.sleep(3)
+					botaobaixar.click()
 					break
-				except:
+				except Exception as e:
+					if debug_mode:
+						print(traceback.format_exc())
 					pass
 			break	
 		except:
-			if contador >= 10:
-				print('#1 Reiniciando página e download')
-				driver.get("https://envios.mercadolivre.com.br/logistics/management-packages")
-				time.sleep(6)
-				# funcao_principal()
-			print('erro ao baixar arquivo')
-			contador = contador + 1
-			pass
-	contador = 0
+			funcaoPrincipal()
+	
 	# Carrega e trata o arquivo
 	while True:
 		time.sleep(1)
 		try:
 			os.chdir(f'C:\\Users\\{user_name}\\Downloads')
-			emrotadeentrega = pd.read_csv([nomesDosArquivos for nomesDosArquivos in os.listdir() if ('logistics_packages' in nomesDosArquivos) and ('.part' not in nomesDosArquivos)][0])
-			os.chdir(diretorio_robo)
-			emrotadeentrega['ID do envio'] = pd.to_numeric(emrotadeentrega['ID do envio'], errors='coerce')
-			emrotadeentrega = emrotadeentrega.loc[~ (emrotadeentrega['ID do envio'].isna())]
-			emrotadeentrega['ID do envio'] = emrotadeentrega['ID do envio'].astype('str').str[:11]
-			emrotadeentrega = emrotadeentrega.fillna('')
-			os.remove(nome_do_arquivo)
-			print('Arquivo em rota de entrega carregado')
-			return emrotadeentrega
-		except Exception as e:
-			print(e)
-			contador = contador + 1
-			if contador >=100:
+			for i in range(20):
+				time.sleep(1)
 				try:
-					os.remove(nome_do_arquivo)
-				except:
+					nomeArquivoGestaoDePacotes = [nomesDosArquivos for nomesDosArquivos in os.listdir() if ('logistics_packages' in nomesDosArquivos) and ('.part' not in nomesDosArquivos)][0]
+					break
+				except Exception as e:
+					if debug_mode:
+						print(traceback.format_exc())
+					if i == 19:
+						raise IndexError
 					pass
-				print('#2 Reiniciando página e download')
-				driver.get("https://envios.mercadolivre.com.br/logistics/management-packages")
-				time.sleep(6)
-				raise KeyError
-			# print('erro ao carregar arquivo')
-			pass
-
-def baixarEntregues():
-	driver.get('https://envios.mercadolivre.com.br/logistics/management-packages')
-	nome_do_arquivo = 'C:\\Users\\' + os.getlogin() + '\\Downloads\\' + 'logistics_packages_' + '-'.join([time.strftime("%d"),time.strftime("%m"),time.strftime("%Y")]) + '.csv'
-	print(nome_do_arquivo)
-	contador = 0
-	# Baixa o arquivo
-	while True:
-		time.sleep(5)
-		try:
-			time.sleep(1)
-			driver.find_element(By.XPATH,'//*[@id="packages-management"]/div[1]/button').click()
-			time.sleep(1)
-			driver.find_element(By.XPATH,'//*[@id="packages-management"]/div[1]/div/div[2]/div/div[3]/div[1]').click()
-			time.sleep(1)
-			driver.find_element(By.XPATH,'/html/body/main/div/div[2]/div/div/div[2]/div[2]/button').click()
-			break	
-		except:
-			if contador >= 10:
-				print('#1 Reiniciando página e download')
-				driver.get("https://envios.mercadolivre.com.br/logistics/management-packages")
-				time.sleep(6)
-			print('erro ao baixar arquivo')
-			contador = contador + 1
-			pass
-	contador = 0
-	# Carrega e trata o arquivo
-	while True:
-		time.sleep(1)
-		try:
-			os.chdir(f'C:\\Users\\{user_name}\\Downloads')
-			entregues = pd.read_csv([nomesDosArquivos for nomesDosArquivos in os.listdir() if ('logistics_packages' in nomesDosArquivos) and ('.part' not in nomesDosArquivos)][0])
+			arquivoGestaoDePacotes = pd.read_csv(nomeArquivoGestaoDePacotes)
+			os.remove(nomeArquivoGestaoDePacotes)
 			os.chdir(diretorio_robo)
-			entregues['ID do envio'] = pd.to_numeric(entregues['ID do envio'], errors='coerce')
-			entregues = entregues.loc[~ (entregues['ID do envio'].isna())]
-			entregues['ID do envio'] = entregues['ID do envio'].astype('str').str[:11]
-			entregues = entregues.fillna('')
-			os.remove(nome_do_arquivo)
-			print('Arquivo baixar entregues carregado')
-			return entregues
+			arquivoGestaoDePacotes['ID do envio'] = pd.to_numeric(arquivoGestaoDePacotes['ID do envio'], errors='coerce')
+			arquivoGestaoDePacotes = arquivoGestaoDePacotes.loc[~ (arquivoGestaoDePacotes['ID do envio'].isna())]
+			arquivoGestaoDePacotes['ID do envio'] = arquivoGestaoDePacotes['ID do envio'].astype('str').str[:11]
+			arquivoGestaoDePacotes = arquivoGestaoDePacotes.fillna('')
+			print('Arquivo "{textodobotaodoarquivo}" carregado')
+			return arquivoGestaoDePacotes
 		except Exception as e:
-			print(e)
-			contador = contador + 1
-			if contador >=100:
-				try:
-					os.remove(nome_do_arquivo)
-				except:
-					pass
-				print('#2 Reiniciando página e download')
-				driver.get("https://envios.mercadolivre.com.br/logistics/management-packages")
-				time.sleep(6)
-				# raise KeyError
-			# print('erro ao carregar arquivo')
-			pass
-
-def baixarFalhaDeEntrega():
-	driver.get('https://envios.mercadolivre.com.br/logistics/management-packages')
-	nome_do_arquivo = 'C:\\Users\\' + os.getlogin() + '\\Downloads\\' + 'logistics_packages_' + '-'.join([time.strftime("%d"),time.strftime("%m"),time.strftime("%Y")]) + '.csv'
-	print(nome_do_arquivo)
-	contador = 0
-	# Baixa o arquivo
-	while True:
-		time.sleep(5)
-		try:
-			time.sleep(1)
-			driver.find_element(By.XPATH,'//*[@id="packages-management"]/div[1]/button').click()
-			time.sleep(1)
-			driver.find_element(By.XPATH,'//*[@id="packages-management"]/div[1]/div/div[2]/div/div[3]/div[2]').click()
-			time.sleep(1)
-			driver.find_element(By.XPATH,'/html/body/main/div/div[2]/div/div/div[2]/div[2]/button').click()
-			break	
-		except:
-			if contador >= 10:
-				print('#1 Reiniciando página e download - baixarfalhadeentrega')
-				driver.get("https://envios.mercadolivre.com.br/logistics/management-packages")
-				time.sleep(6)
-			print('erro ao baixar arquivo')
-			contador = contador + 1
-			pass
-	contador = 0
-	# Carrega e trata o arquivo
-	while True:
-		time.sleep(1)
-		try:
-			os.chdir(f'C:\\Users\\{user_name}\\Downloads')
-			falhaDeEntrega = pd.read_csv([nomesDosArquivos for nomesDosArquivos in os.listdir() if ('logistics_packages' in nomesDosArquivos) and ('.part' not in nomesDosArquivos)][0])
-			os.chdir(diretorio_robo)
-			falhaDeEntrega['ID do envio'] = pd.to_numeric(falhaDeEntrega['ID do envio'], errors='coerce')
-			falhaDeEntrega = falhaDeEntrega.loc[~ (falhaDeEntrega['ID do envio'].isna())]
-			falhaDeEntrega['ID do envio'] = falhaDeEntrega['ID do envio'].astype('str').str[:11]
-			falhaDeEntrega = falhaDeEntrega.fillna('')
-			os.remove(nome_do_arquivo)
-			print(f'Arquivo falha de entrega carregado - baixarfalhadeentrega')
-			return falhaDeEntrega
-		except Exception as e:
-			print(e)
-			contador = contador + 1
-			if contador >=100:
-				try:
-					os.remove(nome_do_arquivo)
-				except:
-					pass
-				print('#2 Reiniciando página e download - baixarfalhadeentrega')
-				driver.get("https://envios.mercadolivre.com.br/logistics/management-packages")
-				time.sleep(6)
-				# raise KeyError
-			# print('erro ao carregar arquivo')
-			pass
+			if debug_mode:
+				print(traceback.format_exc())
+				print(e)
+			raise NotImplementedError
 
 def funcaoPrincipal():
 	while True:
 		try:
 			apagarCSVs()
 
-			# driver.find_element(By.XPATH,'//*[@id="packages-management"]/div[1]/div/div[2]/div/div[3]/div[3]').click()
 			emRotaDeEntrega = baixarArquivoGestaoDePacotes('//*[@id="packages-management"]/div[1]/div/div[2]/div/div[3]/div[3]').values.tolist() 
 			entregue = baixarArquivoGestaoDePacotes('//*[@id="packages-management"]/div[1]/div/div[2]/div/div[3]/div[1]').values.tolist()
 			falhaDeEntrega = baixarArquivoGestaoDePacotes('//*[@id="packages-management"]/div[1]/div/div[2]/div/div[3]/div[2]').values.tolist()		
@@ -274,9 +120,10 @@ def funcaoPrincipal():
 			update_values(ID_PLANILHA_BASE_COCKPIT,'ENTREGUES!A2','USER_ENTERED',entregue)
 			
 			print(f'Última atualização: {agora}')
-			# exit()
+			
 			print('Pausa para acompanhamento...')
-			time.sleep(int(carregarParametros()["delayacompanhamento"])*60)
+			# time.sleep(int(carregarParametros()["delayacompanhamento"])*60)
+			time.sleep(10*60)
 
 		except Exception as e:
 			if debug_mode:
